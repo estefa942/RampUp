382cb98b4dd90a9c1402f3c9bd0c391ef81d8dad
estefa942 <estefa942@gmail.com>
2018-08-04 21:16:05 -0500

    Reservas casi terminada

--- src/app/reservas/reservas.component.ts
+++ src/app/reservas/reservas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IButton } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-reservas',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservas.component.css']
 })
 export class ReservasComponent implements OnInit {
-
+  tables:string[]=["1","2","3","4","5","6","7","más"];
+  reservationOptions:string[]=["Almuerzo","Cena"];
+  numberPersons:string="";
+  reservationOption:string="";
   constructor() { }
-
+  
   ngOnInit() {
   }
+
+ chooseTable(table) {
+   this.numberPersons=table;
+   if (table==="más"){
+     this.numberPersons="+7"
+   } 
+   return this.numberPersons;
+ }
+
+ chooseOptionReservation(reservation){
+     this.reservationOption=reservation;
+     return this.reservationOption;
+ }
 
 }
